ae06a59ce1c650edcf64d883091869c94c4cf260
didier.cheung <didier.cheung@contentsquare.com>
2023-06-06 19:12:28 +0200

    add dumb solution

--- challenges/data-layer/src/index.ts
+++ challenges/data-layer/src/index.ts
@@ -13,4 +13,37 @@ export type DataEntityMap = {
   song: Song;
 };
 
-export class DataStore {}
+export class DataStore {
+  songs: Song[];
+  movies: Movie[];
+  constructor(songs: Song[] = [], movies: Movie[] = []) {
+    this.songs = songs;
+    this.movies = movies;
+  }
+
+  addSong(song: Song) {
+    this.songs.push(song);
+    return song;
+  }
+
+  addMovie(movie: Movie) {
+    this.movies.push(movie);
+    return movie;
+  }
+
+  getAllSongs(): Song[] {
+    return this.songs;
+  }
+
+  getAllMovies(): Movie[] {
+    return this.movies;
+  }
+
+  clearSongs() {
+    this.songs = [];
+  }
+
+  clearMovies() {
+    this.movies = [];
+  }
+}
