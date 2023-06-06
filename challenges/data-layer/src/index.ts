37a26db6527084b65e298b59f8a07b8a9089b457
didier.cheung <didier.cheung@contentsquare.com>
2023-06-06 22:44:30 +0200

    add advanced types

--- challenges/data-layer/src/index.ts
+++ challenges/data-layer/src/index.ts
@@ -13,37 +13,68 @@ export type DataEntityMap = {
   song: Song;
 };
 
-export class DataStore {
-  songs: Song[];
-  movies: Movie[];
-  constructor(songs: Song[] = [], movies: Movie[] = []) {
-    this.songs = songs;
-    this.movies = movies;
-  }
+type DataEntities = keyof DataEntityMap;
 
+type DataStorage = {
+  [K in DataEntities as `${K}s`]: Record<string, DataEntityMap[K]>;
+};
+
+function isDefined<T>(value: T | undefined): value is T {
+  return typeof value !== "undefined";
+}
+
+export type DataStorageMethods = {
+  [K in DataEntities as `add${Capitalize<K>}`]: (
+    arg: DataEntityMap[K]
+  ) => DataEntityMap[K];
+} & {
+  [K in DataEntities as `get${Capitalize<K>}`]: (
+    id: string
+  ) => DataEntityMap[K] | undefined;
+} & {
+  [K in DataEntities as `getAll${Capitalize<K>}s`]: (
+    id: string
+  ) => DataEntityMap[K][];
+} & {
+  [K in DataEntities as `clear${Capitalize<K>}s`]: () => null;
+};
+
+export class DataStore implements DataStorageMethods {
+  #dataStorage: DataStorage = {
+    songs: {},
+    movies: {},
+  };
   addSong(song: Song) {
-    this.songs.push(song);
+    this.#dataStorage.songs[song.id] = song;
     return song;
   }
-
   addMovie(movie: Movie) {
-    this.movies.push(movie);
+    this.#dataStorage.movies[movie.id] = movie;
     return movie;
   }
 
-  getAllSongs(): Song[] {
-    return this.songs;
+  getSong(id: string) {
+    return this.#dataStorage.songs[id];
   }
-
-  getAllMovies(): Movie[] {
-    return this.movies;
+  getMovie(id: string) {
+    return this.#dataStorage.movies[id];
   }
-
   clearSongs() {
-    this.songs = [];
+    this.#dataStorage.songs = {};
+    return null;
   }
-
   clearMovies() {
-    this.movies = [];
+    this.#dataStorage.movies = {};
+    return null;
+  }
+  getAllSongs() {
+    return Object.keys(this.#dataStorage.songs)
+      .map((k) => this.#dataStorage.songs[k])
+      .filter(isDefined);
+  }
+  getAllMovies() {
+    return Object.keys(this.#dataStorage.movies)
+      .map((k) => this.#dataStorage.movies[k])
+      .filter(isDefined);
   }
 }
